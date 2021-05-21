896e72adf52fdccdfb3fedcc46dfeed3cb841bbe
oh980225 <oh980225@gmail.com>
2021-05-21 19:21:25 +0900

    [ADD] JWT인증 및 회원가입, 로그인 초본

---
+++ back/src/main/java/donot/gas/back/controller/UserController.java
@@ -0,0 +1,94 @@
+package donot.gas.back.controller;
+
+import donot.gas.back.auth.JwtTokenProvider;
+import donot.gas.back.dto.ErrorDto;
+import donot.gas.back.dto.JwtDto;
+import donot.gas.back.dto.ResponseDto;
+import donot.gas.back.dto.UserDto;
+import donot.gas.back.entity.User;
+import donot.gas.back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userRepository = userRepository;
+    }
+
+    // 회원가입
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody Map<String, String> user) {
+        try {
+            if (userRepository.findByLoginId(user.get("loginId")).isPresent()) {
+                throw new IllegalArgumentException();
+            }
+            User newUser = new User(user.get("username"), user.get("loginId"), passwordEncoder.encode(user.get("password")), 0L, "USER");
+            userRepository.save(newUser);
+
+            return ResponseEntity.ok(new UserDto(newUser));
+        } catch (IllegalArgumentException e) {
+            return (ResponseEntity<?>) ResponseEntity.badRequest();
+        }
+
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> user) throws Exception {
+        System.out.println(user);
+        System.out.println(userRepository.findByLoginId(user.get("loginId")));
+        User member = userRepository.findByLoginId(user.get("loginId"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        ResponseDto responseDto = ResponseDto.builder()
+                .status(200)
+                .responseMessage("로그인 성공")
+                .data(new JwtDto(jwtTokenProvider.createToken(member.getLoginId(), member.getRole())))
+                .build();
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/api/all")
+    public ResponseEntity<?> findAll(HttpServletRequest request) {
+        List<User> userList = userRepository.findAll();
+        List<UserDto> userDtoList = new ArrayList<>();
+        for (User user : userList) {
+            userDtoList.add(new UserDto(user));
+        }
+
+        System.out.println("사용자 : " + jwtTokenProvider.getUserPk(request.getHeader("X-AUTH-TOKEN")));
+        ResponseDto responseDto = ResponseDto.builder()
+                .status(200)
+                .responseMessage("모든 회원 조회 성공")
+                .data(userDtoList)
+                .build();
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> badRequest(Exception e) {
+        System.err.println(e.getClass());
+        ErrorDto error = ErrorDto.builder()
+                .status(400)
+                .responseMessage("잘못된 입력이 들어왔습니다.")
+                .build();
+        return ResponseEntity.badRequest().body(error);
+    }
+}
---
+++ back/src/main/java/donot/gas/back/service/CustomUserDetatilService.java
@@ -0,0 +1,24 @@
+package donot.gas.back.service;
+
+import donot.gas.back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetatilService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetatilService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        return userRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
